56bcd2a43834350c89478df18a10a7780eec95d9
SakuniSirisena <sakunisirisena8@gmail.com>
2024-12-21 15:34:45 +0530

    commit sakuni

--- lib/screens/get_started.dart
+++ lib/screens/get_started.dart
@@ -6,14 +6,116 @@ class GetStartedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/register');
-          },
-          child: const Text('This is Get Started'),
-        ),
+      body: Stack(
+        children: [
+          // Background
+          Container(
+            decoration: const BoxDecoration(
+              color:Color.fromARGB(255, 163, 240, 219),
+             
+            ),
+          ),
+          // Content
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Illustration
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Container(
+                  height: 200,
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(
+                          './assets/bookmark.webp'), // Add an illustration here
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              // Title
+              const Text(
+                'Read Books\nBe Smart',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color.fromARGB(255, 0, 0, 0),
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 10),
+              // Subtitle (Dummy text)
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30.0),
+                child: Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Color.fromARGB(179, 0, 0, 0),
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              const Spacer(),
+              // Buttons
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 20.0, vertical: 40.0),
+                child: Column(
+                  children: [
+                    // Get Started Button
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/register');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(vertical: 15),
+                        backgroundColor: Colors.teal,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          "Let's Get Started",
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 0, 0, 0),
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    // Sign In Button
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/login');
+                      },
+                      child: const Text(
+                        'Sign In',
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 0, 0, 0),
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: GetStartedScreen(),
+  ));
 }
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,19 +1,193 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<LoginScreen> {
+  bool _isPasswordVisible = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/home');
-          },
-          child: const Text('This is Lssogin'),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        
+        child: Column(
+          
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // Sign In Title
+            const ColoredBox(color: Colors.black12),
+            const Text(
+              'Sign In',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            // Subtitle
+            const Text(
+              'Hi, welcome back, you have been missed',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16, color: Colors.black54),
+            ),
+            const SizedBox(height: 40),
+            // Email Text Field
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+                hintText: 'Enter Your Email',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                filled: true,
+                fillColor: const Color.fromARGB(255, 217, 223, 224),
+              ),
+            ),
+            const SizedBox(height: 20),
+            // Password Text Field
+            TextField(
+              obscureText: !_isPasswordVisible,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                hintText: 'Enter Your Password',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                filled: true,
+                fillColor: Colors.grey[200],
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _isPasswordVisible
+                        ? Icons.visibility
+                        : Icons.visibility_off,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _isPasswordVisible = !_isPasswordVisible;
+                    });
+                  },
+                ),
+              ),
+            ),
+            // Forgot Password
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () {
+                  // Add your 'Forgot Password' action here
+                },
+                child: const Text(
+                  'Forgot Password?',
+                  style: TextStyle(color: Color.fromARGB(255, 28, 147, 151)),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            // Sign In Button
+            ElevatedButton(
+              onPressed: () {
+                // Add your sign-in action here
+                Navigator.pushNamed(context, '/home');
+              },
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                backgroundColor: Colors.green,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+              child: const Text(
+                'Sign In',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            const SizedBox(height: 20),
+            // OR divider
+            const Row(
+              children: [
+                Expanded(child: Divider(thickness: 1)),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Text(
+                    'Or sign in with',
+                    style: TextStyle(color: Colors.black54),
+                  ),
+                ),
+                Expanded(child: Divider(thickness: 1)),
+              ],
+            ),
+            const SizedBox(height: 20),
+            // Social Media Buttons (Apple, Google, Facebook)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Apple Button
+                IconButton(
+                  icon: const Icon(Icons.apple),
+                  onPressed: () {
+                    // Add Apple login action
+                  },
+                  iconSize: 40,
+                  color: Colors.black,
+                ),
+                const SizedBox(width: 20),
+                // Google Button
+                IconButton(
+                  icon: const Icon(Icons.games),
+                  color: Colors.blue, // Replace with your Google logo asset
+                  onPressed: () {
+                    // Add Google login action
+                  },
+                  iconSize: 40,
+                ),
+                const SizedBox(width: 20),
+                // Facebook Button
+                IconButton(
+                  icon: const Icon(Icons.snapchat),
+                  color: const Color.fromARGB(255, 0, 0, 0),
+                  // Replace with your Facebook logo asset
+                  onPressed: () {
+                    // Add Facebook login action
+                  },
+                  iconSize: 40,
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            // Sign Up Link
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Don't have an account? "),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/register');
+                  },
+                  child: const Text(
+                    'Sign Up',
+                    style: TextStyle(color: Colors.green),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: LoginScreen(),
+  ));
 }
